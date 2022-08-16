bdcc5f3f7366d947385351ce603cbae9560c2cd5
znammikhail <znamenskymihail@gmail.com>
2022-08-17 01:28:44 +0300

    add README, docker-compose, gitignore

--- webapp/src/main/tasks.py
+++ webapp/src/main/tasks.py
@@ -1,10 +1,20 @@
-# Create your tasks here
-
-
+from http.client import HTTPResponse
+import imp
+from multiprocessing.spawn import import_main_path
+import requests as r
+import uuid
 from celery import shared_task
+from django.conf import settings
 
+CAT_URL = 'https://cataas.com/cat'
 
 @shared_task
-def add(x, y):
-    return x + y
+def download_a_cat():
+    resp = r.get(CAT_URL)
+    file_ext = resp.headers.get('Content-Type').split('/')[1]
+    file_name = settings.BASE_DIR / 'cats' / (str(uuid.uuid4())+ "." + file_ext)
+    with open(file_name, 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=128):
+            f.write(chunk)
+    return True
 
--- webapp/src/main/views.py
+++ webapp/src/main/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-
+from . import tasks
 
 def home(request):
     # start s task
+    tasks.download_a_cat.delay()
     return HttpResponse('<h1>Гружу кота!<h1>')
 
 
